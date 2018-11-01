46e6f0a2d5f33ee67181d8ccf5c6d03de9d77c1a
Laurent Renard <laurent@Laurents-MacBook-Pro.local>
2018-11-01 15:20:06 -0400

    try fix ci

--- rollup/test.js
+++ rollup/test.js
@@ -2,10 +2,11 @@ import node from 'rollup-plugin-node-resolve';
 import cjs from 'rollup-plugin-commonjs';
 
 export default {
-	input: './test/index.js',
-	output: {
-		format: 'iife',
-		name: 'test'
-	},
-	plugins: [node(), cjs()]
+    input: './test/index.js',
+    output: {
+        file: './test/dist/debug.js',
+        format: 'iife',
+        name: 'test'
+    },
+    plugins: [node(), cjs()]
 };
